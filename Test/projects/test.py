39cc4008c78364a43936ef1cd3024e41a683696a
yhb1226 <3537311140@qq.com>
2026-07-15 20:38:40 +0800

    python学习

--- Test/projects/test.py
+++ Test/projects/test.py
@@ -2,9 +2,9 @@ name = '张三'
 age = '24'
 gender = '男'
 
-output = '他是%s,它的性别是%s,它的年龄是%s' %(name, gender, age)
-
-print(output)
+#output = '他是%s,它的性别是%s,它的年龄是%s' %(name, gender, age)
 
 #f-string
 info = f'他是{name},它的性别是{gender},它的年龄是{age}'
+print(info)
+
