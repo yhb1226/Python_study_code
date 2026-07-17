69c5b2af53e718ea0ea6eeb55c7a77ed43adbbbd
yhb1226 <3537311140@qq.com>
2026-07-18 00:06:54 +0800

    可变位置参数和可变关键字参数

--- Test/projects/test.py
+++ Test/projects/test.py
@@ -7,8 +7,15 @@ a = 1
 #     print('python')
 # print('结束')    
 
-def welcome():
-    print('欢迎')
+def complex_func(a, b, *args, default_val=10, **kwargs):
+    print(f"a={a}, b={b}")
+    print(f"args={args}")
+    print(f"default={default_val}")
+    print(f"kwargs={kwargs}")
 
-for a in range(7):
-    welcome() 
+complex_func(1, 2, 3, 4, 5, default_val=99, x=100, y=200)
+# 输出:
+# a=1, b=2
+# args=(3, 4, 5)
+# default=99
+# kwargs={'x': 100, 'y': 200}
